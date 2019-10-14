5b4247833dd4e8bb609617dadb227bf6477ef61a
soslin <seanoslin@gmail.com>
2019-10-14 16:28:20 -0500

    next commit

--- regression/evaluate.py
+++ regression/evaluate.py
@@ -73,7 +73,11 @@ def regression_errors(y,yhat):
     RMSE = sqrt(mean_squared_error(df['tip'], df.yhat))
     return (SSE, ESS, TSS, MSE, RMSE) #How do i run this?
 
-# Write a function, baseline_mean_errors(y), that takes in your target, y, computes the SSE, MSE & RMSE when yhat is equal to the mean of all y, and returns the error values (SSE, MSE, and RMSE).
+
+
+
+
+#6. Write a function, baseline_mean_errors(y), that takes in your target, y, computes the SSE, MSE & RMSE when yhat is equal to the mean of all y, and returns the error values (SSE, MSE, and RMSE).
 
 def baseline_mean_errors(y):
 
@@ -98,4 +102,61 @@ df_eval
 ESS_baseline = sum((df.yhat - df['tip'].mean())**2)
 SSE_baseline = SSE['baseline_error'] #why is this erring?
 TSS_baseline = ESS_baseline + SSE_baseline
+MSE_baseline = SSE['baseline_error']/len(df)
+RMSE_baseline = sqrt(MSE_baseline)
 
+
+
+#calculating R2 - 2 ways
+#1
+SSE = (df['yhat'] - df['tip'])**2
+ESS = sum((df.yhat - df['tip'].mean())**2) 
+TSS = SSE + ESS 
+MSE = SSE/len(df) 
+RMSE = sqrt(mean_squared_error(df['tip'], df.yhat))
+
+R2 = ESS/TSS
+print('R-squared = ',round(R2,3))
+print("Percent of variance in y explained by x = ", round(R2*100,1), "%")
+
+#2
+r2 = ols_model.rsquared
+print('R-squared = ', round(r2,3))
+
+#F statistic/p-value
+f_pval = ols_model.f_pvalue
+
+print("p-value for model significance = ", round(f_pval,8))
+
+
+
+
+
+#7. Write a function, better_than_baseline(SSE), that returns true if your model performs better than the baseline, otherwise false.
+
+def better_than_baseline(SSE, SSE_baseline):
+    MSE_baseline = mean_squared_error(df_baseline.y, df_baseline.yhat)
+    if MSE < MSE_baseline:
+        return True
+MSE_baseline = mean_squared_error(df_baseline['tip'], df_baseline.yhat)
+print("model is better?", MSE < MSE_baseline)
+
+
+#8. Write a function, model_significance(ols_model), that takes the ols model as input and returns the amount of variance explained in your model (r^2), and the value telling you whether the correlation between the model and the tip value are statistically significant (F stat, value).
+
+
+
+
+def model_significance(ols_model):
+
+r2 = ESS/TSS
+print('R-squared = ',round(r2,3))
+print("Percent of variance in y explained by x = ", round(R2*100,1), "%")
+
+r2 = ols_model.rsquared
+print('r-squared = ', round(r2,3))
+
+#F statistic/p-value
+f_pval = ols_model.f_pvalue
+
+print("p-value for model significance = ", round(f_pval,8))
