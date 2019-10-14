b167dff88c1aa0a13aa4dd44006b887716446257
soslin <seanoslin@gmail.com>
2019-10-13 21:16:12 -0500

    next commits

--- regression/split_scale.py
+++ regression/split_scale.py
@@ -30,7 +30,6 @@ def standard_scaler():
                 .fit(x_train[['monthly_charges', 'tenure']])
     scaler_y_train = StandardScaler(copy=True, with_mean=True, with_std=True)\
                 .fit(pd.DataFrame(y_train))
-    
 #scaler.transform
     train_x_scaled_data = pd.DataFrame(scaler_x_train.transform(x_train), columns = x_train.columns.values).set_index([x_train.index.values])
     test_x_scaled_data = pd.DataFrame(scaler_x_train.transform(x_test), columns = x_test.columns.values).set_index([x_test.index.values])
@@ -43,25 +42,30 @@ train_x_scaled_data, test_x_scaled_data, train_y_scaled_data, test_y_scaled_data
 
 
 #scale_inverse()
+# train_unscaled = pd.DataFrame(scaler.inverse_transform(train_scaled), columns=train_scaled.columns.values).set_index([train.index.values])
+# test_unscaled = pd.DataFrame(scaler.inverse_transform(test_scaled), columns=test_scaled.columns.values).set_index([test.index.values])
 def scale_inverse(x_train, x_test, y_train, y_test):
-    inverse_scaler_x_train = train_x_scaled_data.inverse_transform(x_train[['monthly_charges', 'tenure']])
-    inverse_scaler_x_test = test_x_scaled_data.inverse_transform(x_test[['monthly_charges', 'tenure']])
-    inverse_scaler_y_train = train_y_scaled_data.inverse_transform(pd.DataFrame(y_train))
-    inverse_scaler_y_test = test_y_scaled_data.inverse_transform(pd.DataFrame(y_test))
-    return inverse_scaler_x_train, inverse_scaler_x_test, inverse_scaler_y_train, inverse_scaler_y_test  
+    inverse_scaler_x_train = pd.DataFrame(scaler_x_train.inverse_transform(train_x_scaled_data), columns = x_train.columns.values).set_index([x_train.index.values])
+    inverse_scaler_x_test = pd.DataFrame(scaler_x_train.inverse_transform(test_x_scaled_data), columns = x_test.columns.values).set_index([x_test.index.values])
+
+#why is this wrong?
+    # inverse_scaler_y_train = pd.DataFrame(scaler_y_train.inverse_transform(train_y_scaled_data), columns = y_train.columns.values).set_index[y_train.index.values])
+
+    # inverse_scaler_y_test = test_y_scaled_data.inverse_transform(pd.DataFrame(y_test))
+    
+    # return inverse_scaler_x_train, inverse_scaler_x_test, inverse_scaler_y_train, inverse_scaler_y_test  
     
 
 #uniform_scaler()
-def uniform_scaler(x_train, x_test, y_train, y_test):
+def uniform_scaler():
+    x_train, x_test, y_train, y_test = split_my_data()
     u_scaler_x_train = QuantileTransformer(n_quantiles=100, output_distribution='uniform', random_state=123, copy=True).fit(x_train[['monthly_charges', 'tenure']])
-    u_scaler_x_test = QuantileTransformer(n_quantiles=100, output_distribution='uniform', random_state=123, copy=True).fit(x_test[['monthly_charges', 'tenure']])
     u_scaler_y_train = QuantileTransformer(n_quantiles=100, output_distribution='uniform', random_state=123, copy=True).fit(pd.DataFrame(y_train))
-    u_scaler_y_test = QuantileTransformer(n_quantiles=100, output_distribution='uniform', random_state=123, copy=True).fit(pd.DataFrame(y_test))
 
-    u_train_x_scaled = pd.DataFrame(u_scaler_x_train.transform(x_train[['monthly_charges', 'tenure']]))
-    u_test_x_scaled = pd.DataFrame(u_scaler_x_test.transform(x_test[['monthly_charges', 'tenure']]))
-    u_train_y_scaled = pd.DataFrame(u_scaler_y_train.transform(pd.DataFrame(y_train)))
-    u_test_y_scaled = pd.DataFrame(u_scaler_y_test.transform(pd.DataFrame(y_test)))
+    u_train_x_scaled = pd.DataFrame(u_scaler_x_train.transform(x_train), columns = x_train.columns.values).set_index([x_train.index.values])
+    u_test_x_scaled = pd.DataFrame(u_scaler_x_train.transform(x_test), columns = x_test.columns.values).set_index([x_test.index.values])
+    u_train_y_scaled = pd.DataFrame(u_scaler_y_train.transform(y_train), columns = y_train.columns.values).set_index([y_train.index.values])
+    u_test_y_scaled = pd.DataFrame(u_scaler_y_train.transform(y_test), columns = y_test.columns.values).set_index([y_test.index.values])
     return u_train_x_scaled, u_test_x_scaled, u_train_y_scaled, u_test_y_scaled
 
 
@@ -70,30 +74,24 @@ def uniform_scaler(x_train, x_test, y_train, y_test):
 #gaussian_scaler()
 def gaussian_scaler(x_train, x_test, y_train, y_test):
     g_x_train_scaler =PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(x_train[['monthly_charges', 'tenure']])
-    g_x_test_scaler =PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(x_test[['monthly_charges', 'tenure']])
     g_y_train_scaler =PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(pd.DataFrame(y_train))
-    g_y_test_scaler =PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(pd.DataFrame(y_test))
-
-    g_x_train_scaled = pd.DataFrame(g_x_train_scaler.transform(x_train[['monthly_charges', 'tenure']]))
-    g_x_test_scaled = pd.DataFrame(g_x_test_scaler.transform(x_test[['monthly_charges', 'tenure']]))
-    g_y_train_scaled = pd.DataFrame(g_y_train_scaler.transform(pd.DataFrame(y_train)))
-    g_y_test_scaled = pd.DataFrame(g_y_test_scaler.transform(pd.DataFrame(y_test)))
+    
+    g_x_train_scaled = pd.DataFrame(g_x_train_scaler.transform(x_train), columns = x_train.columns.values).set_index([x_train.index.values])
+    g_x_test_scaled = pd.DataFrame(g_x_train_scaler.transform(x_test), columns = x_test.columns.values).set_index([x_test.index.values])
+    g_y_train_scaled = pd.DataFrame(g_y_train_scaler.transform(y_train), columns = y_train.columns.values).set_index([y_train.index.values])
+    g_y_test_scaled = pd.DataFrame(g_y_train_scaler.transform(y_test), columns = y_test.columns.values).set_index([y_test.index.values])
     return g_x_train_scaled, g_x_test_scaled, g_y_train_scaled, g_y_test_scaled
-
-
 
 
 #min_max_scaler()
 def min_max_scaler(x_train, x_test, y_train, y_test):
     mm_x_train_scaler = MinMaxScaler(copy=True, feature_range=(0,1)).fit(x_train[['monthly_charges', 'tenure']])
-    mm_x_test_scaler = MinMaxScaler(copy=True, feature_range=(0,1)).fit(x_test[['monthly_charges', 'tenure']])
     mm_y_train_scaler = MinMaxScaler(copy=True, feature_range=(0,1)).fit(pd.DataFrame(y_test))
-    mm_y_test_scaler = MinMaxScaler(copy=True, feature_range=(0,1)).fit(pd.DataFrame(y_test))
-
-    mm_x_train_scaled = pd.DataFrame(mm_x_train_scaler.transform(x_train[['monthly_charges', 'tenure']]))
-    mm_x_test_scaled = pd.DataFrame(mm_x_test_scaler.transform(x_test[['monthly_charges', 'tenure']]))
-    mm_y_train_scaled = pd.DataFrame(mm_y_train_scaler.transform(pd.DataFrame(y_train)))
-    mm_y_test_scaled = pd.DataFrame(mm_y_test_scaler.transform(pd.DataFrame(y_test)))
+    
+    mm_x_train_scaled = pd.DataFrame(mm_x_train_scaler.transform(x_train), columns = x_train.columns.values).set_index([x_train.index.values])
+    mm_x_test_scaled = pd.DataFrame(mm_x_train_scaler.transform(x_test), columns = x_test.columns.values).set_index([x_test.index.values])
+    mm_y_train_scaled = pd.DataFrame(mm_y_train_scaler.transform(y_train), columns = y_train.columns.values).set_index([y_train.index.values])
+    mm_y_test_scaled = pd.DataFrame(mm_y_train_scaler.transform(y_test), columns = y_test.columns.values).set_index([y_test.index.values])
     return mm_x_train_scaled, mm_x_test_scaled, mm_y_train_scaled, mm_y_test_scaled
 
 
@@ -101,13 +99,11 @@ def min_max_scaler(x_train, x_test, y_train, y_test):
 #iqr_robust_scaler()
 def iqr_robust_scaler(x_train, x_test, y_train, y_test):
     iqr_x_train_scaler = RobustScaler(quantile_range=(25.0,75.0), copy=True, with_centering=True, with_scaling=True).fit(x_train[['monthly_charges', 'tenure']])
-    iqr_x_test_scaler = RobustScaler(quantile_range=(25.0,75.0), copy=True, with_centering=True, with_scaling=True).fit(x_test[['monthly_charges', 'tenure']])
     iqr_y_train_scaler = RobustScaler(quantile_range=(25.0,75.0), copy=True, with_centering=True, with_scaling=True).fit(pd.DataFrame(y_test))
-    iqr_y_test_scaler = RobustScaler(quantile_range=(25.0,75.0), copy=True, with_centering=True, with_scaling=True).fit(pd.DataFrame(y_test))
 
-    iqr_x_train_scaled = pd.DataFrame(iqr_x_train_scaler.transform(x_train[['monthly_charges', 'tenure']]))
-    iqr_x_test_scaled = pd.DataFrame(iqr_x_test_scaler.transform(x_test[['monthly_charges', 'tenure']]))
-    iqr_y_train_scaled = pd.DataFrame(iqr_y_train_scaler.transform(pd.DataFrame(y_train)))
-    iqr_y_test_scaled = pd.DataFrame(iqr_y_test_scaler.transform(pd.DataFrame(y_test)))
+    iqr_x_train_scaled = pd.DataFrame(iqr_x_train_scaler.transform(x_train), columns = x_train.columns.values).set_index([x_train.index.values])
+    iqr_x_test_scaled = pd.DataFrame(iqr_x_train_scaler.transform(x_test), columns = x_test.columns.values).set_index([x_test.index.values])
+    iqr_y_train_scaled = pd.DataFrame(iqr_y_train_scaler.transform(y_train), columns = y_train.columns.values).set_index([y_train.index.values])
+    iqr_y_test_scaled = pd.DataFrame(iqr_y_train_scaler.transform(y_test), columns = y_test.columns.values).set_index([y_test.index.values])
     return iqr_x_train_scaled, iqr_x_test_scaled, iqr_y_train_scaled, iqr_y_test_scaled
 
