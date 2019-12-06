fbeea2de2aa6283f05d2a6cd95906c4cf24ee01d
soslin <seanoslin@gmail.com>
2019-12-06 13:13:03 -0600

    Next commite

--- nlp/prepare.py
+++ nlp/prepare.py
@@ -11,12 +11,23 @@ import pandas as pd
 import acquire
 
 
-def basic_clean():
+def handle_white_space(s):
+    s = s.strip()
+    s = s.replace('\n', ' ')
+    return re.sub(r"[^a-z0-9'\s]", '', s)
+
+def unicode_normalize(s): 
+    return unicodedata.normalize('NFKD', s)\
+    .encode('ascii', 'ignore')\
+    .decode('utf-8', 'ignore')
+
+def prepare_article():
     original = acquire.make_new_request()
     article = original.copy()
     article['body'] = article.body.str.lower()
-    article['title'] = article.title.str.lower()
+    article['body'] = article.body.apply(handle_white_space)
+    article['body'] = article.body.apply(unicode_normalize)
     return article
-article = basic_clean()
+article = prepare_article()
 
 
